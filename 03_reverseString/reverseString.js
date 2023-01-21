3345548873ab62b690eb8967241a43bd3b027bb7
dylanwheatley <dylan.r.wheatley@gmail.com>
2023-01-21 16:00:15 -0500

    Exercise 3 all tests successful

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let splitWord = word.split("");
 
+    let reverseWord = splitWord.reverse();
+
+    let joinedWord = reverseWord.join("");
+
+    return joinedWord;
 };
 
 // Do not edit below this line
