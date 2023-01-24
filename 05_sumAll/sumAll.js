5128459886ce1998ca66a3055ba51d3f2af23d5e
dylanwheatley <dylan.r.wheatley@gmail.com>
2023-01-23 20:13:45 -0500

    Tests for sumAll successful

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let sum = num1 + num2;
+    let subNum = 0;
+    
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR"
+
+    if (num1 > num2) { // 3, 2
+        let temp = num2;// temp is 2
+        num2 = num1;//num2 is now 3
+        num1 = temp; //num1 is now 2
+        
+    }
+
+    subNum = num2; //should be the bigger num no matter what
+
+    for (let i=0; i<num2; i++)
+        if (num1 != (subNum-1)) {
+            subNum -= 1;
+            sum += subNum;
+        }
+
+        return sum;
 };
+
+console.log(sumAll(4, 1));
+
 
 // Do not edit below this line
 module.exports = sumAll;
