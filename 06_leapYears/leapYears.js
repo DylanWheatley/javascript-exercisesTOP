15546302edf1a8afa8ab661fe9dd90861b9053e2
dylanwheatley <dylan.r.wheatley@gmail.com>
2023-01-24 13:06:00 -0500

    Tests successful for leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,30 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
-};
+    //number needs to be divisible by 4
+    //number can also be divisible by 400, but not also 100
+
+    if (year % 4 === 0){
+        if (year % 400===0) {
+            return true;
+        }
+        else if (year % 100 === 0 && year % 400 != 0) {
+            return false;
+        }
+        else if (year % 4 === 0 && year % 100 != 0) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+        
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(leapYears(1800));
 
 // Do not edit below this line
 module.exports = leapYears;
