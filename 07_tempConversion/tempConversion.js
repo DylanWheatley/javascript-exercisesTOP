0b3733a4ca58cb187f395c9e7a88068a0b10cad4
dylanwheatley <dylan.r.wheatley@gmail.com>
2023-01-24 13:24:22 -0500

    All tests successful for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+
+const convertToCelsius = function(tempToC) {
+
+    tempC = (tempToC - 32) / 1.8;
+
+    let roundC = Math.round(tempC * 10) / 10;
+
+    return roundC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempToF) {
+
+    tempF = (tempToF * 1.8) + 32;
+    
+    let roundF = Math.round(tempF * 10) /10
+
+    return roundF;
 };
 
 // Do not edit below this line
