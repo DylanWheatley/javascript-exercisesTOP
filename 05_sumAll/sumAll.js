940d29c383b3b04eb475358cb80334169253a667
dylanwheatley <dylan.r.wheatley@gmail.com>
2023-01-24 13:07:02 -0500

    Small changes to format

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,10 +3,10 @@ const sumAll = function(num1, num2) {
     let sum = num1 + num2;
     let subNum = 0;
     
-    if (num1 < 0 || num2 < 0) return "ERROR";
-    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR"
+    if (num1 < 0 || num2 < 0) return "ERROR"; //no negative numbers
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR" //no non-number entries
 
-    if (num1 > num2) { // 3, 2
+    if (num1 > num2) { // ex. 3, 2
         let temp = num2;// temp is 2
         num2 = num1;//num2 is now 3
         num1 = temp; //num1 is now 2
